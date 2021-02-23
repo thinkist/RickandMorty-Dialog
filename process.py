d770dd683fcc25b2352c66417e96eea33223ff48
thinkist <thinkist@users.noreply.github.com>
2021-02-23 19:42:11 +0800

    Fix

--- process.py
+++ process.py
@@ -8,6 +8,6 @@ for i, m in a.items():
     folder = 'Season_%d'%i
     os.system('mkdir '+folder)
     for name, content in m.items():
-        with open('%s/%s.md'%(folder, name), 'w') as f:
+        with open('%s/%s.txt'%(folder, name), 'w') as f:
             f.write('# %s\n'%name)
             f.write(content)
